e1cb343f38b5ad03963e0d3c08e7b4f72830949a
Roger Butenuth <roger.butenuth@codecentric.de>
2023-10-26 16:15:03 +0200

    Changed versions to match Mule 4.4

--- src/main/java/de/codecentric/dwcaller/utils/WeavePathProtocolHandler.java
+++ src/main/java/de/codecentric/dwcaller/utils/WeavePathProtocolHandler.java
@@ -1,7 +1,6 @@
 package de.codecentric.dwcaller.utils;
 
 import java.io.InputStream;
-import java.nio.charset.Charset;
 
 import org.mule.weave.v2.model.service.ReadFunctionProtocolHandler;
 import org.mule.weave.v2.module.reader.SourceProvider;
@@ -22,12 +21,25 @@ public class WeavePathProtocolHandler implements ReadFunctionProtocolHandler {
 		return url.startsWith(CLASSPATH_PREFIX);
 	}
 
+/* for Mule 4.5 (dw runtime 2.6):
 	@Override
 	public SourceProvider createSourceProvider(String url, LocationCapable locatable, Charset charset) {
 		String uri = url.substring(CLASSPATH_PREFIX.length());
 		Option<InputStream> maybeResource = path.resolve(uri);
 		if (maybeResource.isDefined()) {
 			return SourceProvider.apply(maybeResource.get(), charset);
+		} else {
+			return (SourceProvider) Option.empty();
+		}
+	}
+*/
+	
+	@Override
+	public SourceProvider createSourceProvider(String url, LocationCapable locatable) {
+		String uri = url.substring(CLASSPATH_PREFIX.length());
+		Option<InputStream> maybeResource = path.resolve(uri);
+		if (maybeResource.isDefined()) {
+			return SourceProvider.apply(maybeResource.get());
 		} else {
 			return (SourceProvider) Option.empty();
 		}
